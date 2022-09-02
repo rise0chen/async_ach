a409ca3b5b4a71b45ef1969df9025599a734312c
rise0chen <rise0chen@163.com>
2022-09-02 15:44:39 +0800

    feat: linked waker

--- waker/src/list.rs
+++ waker/src/list.rs
@@ -1,3 +1,5 @@
+//! Only work on single thread
+
 use crate::WakerEntity;
 use ach_linked::LinkedList;
 pub use ach_linked::Node;
@@ -27,21 +29,21 @@ impl<T> WakerList<T> {
     ///
     /// Returns false if the list is empty.
     pub fn wake_one(&self) -> bool {
-        if let Some(mut waker) = self.list.take_all() {
-            loop {
+        if let Some(list) = self.list.take_all() {
+            let mut waker = unsafe { &mut *(list as *mut Node<WakerEntity<T>>) };
+            let ret = loop {
                 if waker.wake() {
-                    if let Some(list) = waker.next() {
-                        unsafe { self.list.push_list(list) };
-                    }
-                    return true;
+                    break true;
                 } else {
                     if let Some(list) = waker.next() {
                         waker = list
                     } else {
-                        return false;
+                        break false;
                     }
                 }
-            }
+            };
+            unsafe { self.list.push_list(list) };
+            ret
         } else {
             false
         }
@@ -51,12 +53,43 @@ impl<T> WakerList<T> {
     /// returns the number of had waked
     pub fn wake_all(&self) -> usize {
         let mut num = 0;
-        let mut list = self.list.take_all();
-        while let Some(waker) = list {
-            waker.wake();
-            list = waker.next();
-            num += 1;
+        if let Some(list) = self.list.take_all() {
+            let mut waker = unsafe { &mut *(list as *mut Node<WakerEntity<T>>) };
+            loop {
+                if waker.wake() {
+                    num += 1;
+                }
+                if let Some(list) = waker.next() {
+                    waker = list
+                } else {
+                    break;
+                }
+            }
+            unsafe { self.list.push_list(list) };
         }
         num
+    }
+    /// Retains only the elements specified by the predicate.
+    ///
+    /// In other words, remove all elements e such that f(&e) returns false.
+    /// This method operates in place, visiting each element exactly once in the original order,
+    /// but not preserves the order of the retained elements.
+    pub fn retain(&self, mut f: impl FnMut(&WakerEntity<T>) -> bool) {
+        let list = self.list.take_all();
+        if let Some(list) = list {
+            let mut remain: Option<&mut Node<WakerEntity<T>>> = None;
+            for waker in list.into_iter() {
+                if f(&*waker) {
+                    if let Some(remain) = &mut remain {
+                        unsafe { remain.push(waker) };
+                    } else {
+                        remain = Some(waker);
+                    }
+                }
+            }
+            if let Some(remain) = remain {
+                unsafe { self.list.push_list(remain) };
+            }
+        }
     }
 }
